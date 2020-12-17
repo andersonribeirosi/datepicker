90db6c8ce7885a8f4a89f2a0238d9ec6b8d08a29
Anderson <andersonribeiro.si@hotmail.com>
2020-12-16 23:12:14 -0300

    Ngbdatepicker com valor definido no input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
-
+import {NgbDateStruct, NgbCalendar, NgbDate} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +9,18 @@ import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 export class AppComponent {
   title = 'datepicker';
-
-  datePick: NgbDateStruct;
+  model: NgbDateStruct;
+  fromDate: NgbDate | null;
   date: {year: number, month: number};
+  toDate: NgbDate | null;
 
   constructor(private calendar: NgbCalendar) {
+    this.toDate = calendar.getPrev(calendar.getToday(), 'd', 7)
+    this.fromDate = calendar.getNext(calendar.getToday(), 'd', 10);
   }
-
-  selectToday() {
-    this.datePick = this.calendar.getToday();
+  
+  ngOnInit() {
   }
-
-  displayMonths = 1;
-  navigation = 'select';
-  showWeekNumbers = false;
-  outsideDays = 'visible';
-  weekStart = 0;
+  
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,30 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateParserFormatter, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { NgbDatepickerComponent } from './services/ngb-datepicker/ngb-datepicker.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 registerLocaleData(localePt);
+import {NgbDatePTParserFormatter} from 'src/app/services/NgbDatePTParserFormatter'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbDatepickerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
+  exports:[NgbDatepickerComponent],
   providers: [
-    [{ provide: LOCALE_ID, useValue: 'pt'}]
+    [{ provide: LOCALE_ID, useValue: 'pt'}],
+    [{provide: NgbDateParserFormatter, useClass: NgbDatePTParserFormatter}],
   ],
   bootstrap: [AppComponent]
 })
